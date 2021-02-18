cf6c35e546a08cf8bf4d389a489071140dccf78e
ScrewThisNoise <51236954+ScrewThisNoise@users.noreply.github.com>
2021-02-18 02:20:43 +0100

    Create Logger.cs

---
+++ SharedCode/Logger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace PS4MacroScripts
+{
+    public static class Logger
+    {
+        public static StreamWriter MacroLogger { get; set; }
+
+        public static void Initiate(string Script)
+        {
+            try
+            {
+                MacroLogger = new StreamWriter($"{Script}.log") {AutoFlush = true};
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public static void Writer(string LogMessage)
+        {
+            string time = DateTime.Now.ToString("h:mm:ss tt");
+            try
+            {
+                MacroLogger.WriteLine($"{time} : {LogMessage}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public static void Destroy()
+        {
+            try
+            {
+                MacroLogger.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
